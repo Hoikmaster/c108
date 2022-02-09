440e039be477ff4898151f754b75f6205bf71e5c
Hoikmaster <72697675+Hoikmaster@users.noreply.github.com>
2022-02-08 18:11:08 -0600

    Update data.py

--- data.py
+++ data.py
@@ -3,9 +3,5 @@ import pandas as pd
 import plotly.figure_factory as ff
 
 df = pd.read_csv("data.csv")
-fig = ff.create_distplot([df["Mobile Brand"].tolist()],["Mobile Brand"],show_hist=False)
-fig.show()
-
-df = pd.read_csv("data.csv")
-fig = ff.create_distplot([df["Average Rating)"].tolist()],["Average Rating"],show_hist=False)
+fig = ff.create_distplot([df["Average Rating)"].tolist()],["Average Rating"])
 fig.show()
